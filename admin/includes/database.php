28926d764f1038fbe52c4a7584df54143cd98cfd
Vincent Chan <vincentcahn1224@gmail.com>
2024-07-20 00:40:51 -0400

    New project

---
+++ admin/includes/database.php
@@ -0,0 +1,10 @@
+<?php
+
+$connect = mysqli_connect( 
+    "sql3.freemysqlhosting.net", // Host
+    "sql3720497", // Username
+    "jJJR3zpj3y", // Password
+    "sql3720497" // Database
+);
+
+mysqli_set_charset( $connect, 'UTF8' );
